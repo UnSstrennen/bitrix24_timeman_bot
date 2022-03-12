134d4e70886f8b2413b0784697df1090fbb1c69d
Un ẞtrennen <41056121+UnSstrennen@users.noreply.github.com>
2022-03-12 17:50:24 +0300

    Update config_TEMPLATE.py

--- config_TEMPLATE.py
+++ config_TEMPLATE.py
@@ -1,8 +1,11 @@
-BASE_HOST = 'https://bitrix.keklol.ru/'
+from os import environ as env
 
-TOKEN = ''
 
-DAY_MIN_LENGTH = 27000
-DAY_MAX_LENGTH = 32400
+BASE_HOST = env.get('BASE_HOST', 'https://bitrix.keklol.ru/')
+
+TOKEN = env.get('TOKEN', '')
+
+DAY_MIN_LENGTH = int(env.get('DAY_MIN_LENGTH', 27000))
+DAY_MAX_LENGTH = int(env.get('DAY_MAX_LENGTH', 32400))
 
 RU_STATES = {'OPENED': 'открыт', 'EXPIRED': 'вы не закрыли предыдущий рабочий день', 'CLOSED': 'закрыт', 'PAUSED': 'приостановлен'}
